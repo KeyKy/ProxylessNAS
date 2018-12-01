5c67a680471d996d8c30cf64b15727171f800f3f
Ligeng Zhu <lykensyu+gitlab@gmail.com>
2018-12-01 18:10:02 -0500

    public release

--- proxyless_nas/model_zoo.py
+++ proxyless_nas/model_zoo.py
@@ -7,40 +7,13 @@ from proxyless_nas.utils import download_url, load_url
 from .nas_modules import MobilenetV1
 
 
-def load_init(expdir):
-    init_path = '%s/init' % expdir
-    assert os.path.isfile(init_path)
-
-    if torch.cuda.is_available():
-        checkpoint = torch.load(init_path)
-    else:
-        checkpoint = torch.load(init_path, map_location='cpu')
-    return checkpoint
-
-
-def load_net(expdir, print_info=False):
-    net_config_path = '%s/net.config' % expdir
-    assert os.path.isfile(net_config_path), 'No net configs found in <%s>' % expdir
-    net_config_json = json.load(open(net_config_path, 'r'))
-    if print_info:
-        print('Net config:')
-        for k, v in net_config_json.items():
-            if k != 'blocks':
-                print('\t%s: %s' % (k, v))
-    net = MobilenetV1.build_from_config(net_config_json)
-    if 'bn' in net_config_json:
-        net.set_bn_param(bn_momentum=net_config_json['bn']['momentum'], bn_eps=net_config_json['bn']['eps'])
-    else:
-        net.set_bn_param(bn_momentum=0.1, bn_eps=1e-3)
-    return net
-
-
 def proxyless_base(pretrained=True, net_config=None, net_weight=None):
-
     assert net_config is not None, "Please input a network config"
     net_config_path = download_url(net_config)
     net_config_json = json.load(open(net_config_path, 'r'))
     net = MobilenetV1.build_from_config(net_config_json)
+
+    # switch to TensorFlow style BN
     if 'bn' in net_config_json:
         net.set_bn_param(bn_momentum=net_config_json['bn']['momentum'], bn_eps=net_config_json['bn']['eps'])
     else:
